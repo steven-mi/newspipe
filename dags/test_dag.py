5d73cab4b713e1796b197ce168580036732afe1c
steven-mi <stevenmi@live.de>
2020-08-08 01:07:05 +0200

    change schedule interval to None in test_dag

--- dags/test_dag.py
+++ dags/test_dag.py
@@ -3,7 +3,7 @@ import os
 from dag_factory import create_dag
 
 url = "bild.de"
-airflow_config = {}
+airflow_config = {'schedule_interval': None}
 
 DAG = create_dag(url=url,
                  airflow_config=airflow_config,
