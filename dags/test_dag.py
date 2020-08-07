b338b53958c39dd5fe44bc2d748e330385f6683d
steven-mi <stevenmi@live.de>
2020-08-08 00:57:19 +0200

    add a test dag

---
+++ dags/test_dag.py
@@ -0,0 +1,10 @@
+import os
+
+from dag_factory import create_dag
+
+url = "bild.de"
+airflow_config = {}
+
+DAG = create_dag(url=url,
+                 airflow_config=airflow_config,
+                 name=os.path.basename(__file__))
