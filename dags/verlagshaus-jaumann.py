843c2eb1ee0dfac3b546107fdc1a633098294f0a
steven-mi <stevenmi@live.de>
2020-08-26 19:46:39 +0200

    added news sources

---
+++ dags/verlagshaus-jaumann.py
@@ -0,0 +1,15 @@
+import os
+    import datetime
+
+    from dag_factory import create_dag
+
+    
+url = "verlagshaus-jaumann.de"
+
+    airflow_config = {'schedule_interval': '*/30 * * * *', # every 30 minutes
+                      'start_date': datetime.datetime(2020, 7, 6, 21), # year, month, day, hour
+                      }
+
+    DAG = create_dag(url=url,
+                     airflow_config=airflow_config,
+                     name=os.path.basename(__file__))
