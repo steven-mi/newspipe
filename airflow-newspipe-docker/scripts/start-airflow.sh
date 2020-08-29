ff449b96aa2dd81bc72e92876cdcae5f2673f954
steven-mi <stevenmi@live.de>
2020-08-29 20:40:13 +0200

    add para

--- airflow-newspipe-docker/scripts/start-airflow.sh
+++ airflow-newspipe-docker/scripts/start-airflow.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
-rm /airflow/airflow-webserver*.pid
+airflow resetdb --yes
+airflow initdb
+
+python3 /scripts/create_account.py
+
+if [ -d "/output/pipelines" ]
+then
+mkdir /output/pipelines_backup/$(date +"%d-%m-%Y")
+cp -r /output/pipelines /output/pipelines_backup/$(date +"%d-%m-%Y")
+rm -rf /output/pipelines
+fi
+
+rm $AIRFLOW_HOME/airflow-webserver*
 airflow webserver -D
 airflow scheduler
